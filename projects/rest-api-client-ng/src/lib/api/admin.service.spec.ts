9862238aa82387a044db640aa41a8d87fb78dea1
Ara Winters <arawinters@users.noreply.github.com>
2024-11-25 09:32:08 -0800

    update the version of angular required (#333)
    * angular updated to @16.x
    
    * angular updated to @17
    
    * angular updated to @17
    
    * angular updated to @17
    
    * angular updated to @18
    
    * zone.js refs updated

--- projects/rest-api-client-ng/src/lib/api/admin.service.spec.ts
+++ projects/rest-api-client-ng/src/lib/api/admin.service.spec.ts
@@ -3,15 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { AdminService } from './admin.service';
 import { ApiModule } from '../api.module';
 import { Configuration } from '../configuration';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 //
 describe('AdminService', () => {
     const confFactory = () => {
         return new Configuration({ basePath: '/'});
     }
     beforeEach(() => TestBed.configureTestingModule({
-        imports: [ApiModule.forRoot( confFactory ), HttpClientModule ]
-    }));
+    imports: [ApiModule.forRoot(confFactory)],
+    providers: [provideHttpClient(withInterceptorsFromDi())]
+}));
 
     it('should be created', () => {
         const service: AdminService = TestBed.get(AdminService);
